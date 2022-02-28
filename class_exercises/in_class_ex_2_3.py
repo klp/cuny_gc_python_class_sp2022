5ba75047dbaf050eb6b2625d9ed71129b60054f6
Kai Prenger <kaiprenger@bom.local>
2022-02-28 17:49:26 -0500

    in class exercise

---
+++ class_exercises/in_class_ex_2_3.py
@@ -0,0 +1,26 @@
+fruits = ['apple', 'banana', 'chair', 'watermelon', 'pineapple', 'Greece']
+
+more_fruits =['strawberry', 'mango', 'kiwi']
+
+best_fruit = 'guava'
+
+fruit_list = []
+
+# Best fruit first element of this list
+# Delete non-fruit items
+# Add more_fruits to fruits
+# print the list 
+# print the length of the list
+
+fruit_list.append(best_fruit)
+fruits.pop()
+fruits.extend(more_fruits)
+fruit_list.extend(fruits)
+print(fruit_list)
+print(len(fruit_list))
+
+
+
+
+
+
--- class_exercises/python_fundamentals_2_3.py
+++ class_exercises/python_fundamentals_2_3.py
@@ -3,7 +3,7 @@
 # Collections used to store multiple items in a single variable. They are
 # ordered, changable, and allows duplicates
 
-from re import M
+from itertools import count
 
 
 my_list = [0, 1, "2", 3, "4", 5, 6]
@@ -31,6 +31,48 @@ my_list.append(7)           # add a value to the end of the list
 my_list.append('banana')
 print(my_list)
 
-# insert
+# Insert
 my_list.insert(0, 'apple')  # takes two arguments, first index, then value
 print(my_list)
+
+my_list.insert(4, 'this is the fifth item')
+print(my_list)
+
+# Add list to another list, embedded
+# Useful in text analysis
+list_2 = [8, 9, 10]
+my_list.append(list_2)
+print(my_list)
+print(my_list[-1])
+# Appending is most common list operation
+
+# Add individual items to a list
+my_list.extend([11, 12, 13])
+print(my_list)
+
+countries = ['brazil', 'bolivia', 'argentina']
+other_countries = ['argentina', 'paraguia']
+countries.append(other_countries)
+print(countries)
+
+print(type(countries[3]))
+print(type(countries[2]))
+
+print(countries[3][1]) # getting item of nested list using indexes
+
+# Removing items
+countries.pop()     # pop off the last item of a list
+print(countries)
+
+countries.remove('bolivia') # remove by content, removes first occurance
+print(countries)
+
+# rare
+quick_list = [1, 2, 3, 1, 2, 3, 1]
+del(quick_list[3])  # if you don't give the index, it'll delete whole list
+print(quick_list)
+
+# clear out a list of all values
+quick_list.clear()  # empties list
+print(quick_list)
+
