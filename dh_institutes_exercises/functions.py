48f4a61caba699bbbfa2e4974052101f54acb4ee
Kai Prenger <kaiprenger@bom.local>
2022-02-28 16:44:32 -0500

    in progress

---
+++ dh_institutes_exercises/functions.py
@@ -0,0 +1,17 @@
+def add_one(x):
+    print(x + 1)
+
+add_one(2)
+add_one(155)
+add_one(5)
+add_one(-1)
+
+def greet():
+    print("Hello! How are you today?")
+
+greet()
+
+def new_greet(name):
+    print (f"Hello, {name}! How are you today?")
+
+new_greet("Kai")
