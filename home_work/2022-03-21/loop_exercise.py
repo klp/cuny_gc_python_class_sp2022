e80c55ffbec2c89bf6fb73b368e55ac248147660
Kai Prenger <kaiprenger@bom.local>
2022-03-17 09:57:31 -0400

    fix folders

---
+++ home_work/2022-03-21/loop_exercise.py
@@ -0,0 +1,13 @@
+""" Create a function called 'only_lower' that takes a list of strings as an argument
+and returns another list. This new list will contain only the lowered version of the words of the previous list (exclude numbers and punctuation)
+
+Hint:
+- google "python isalpha" and play a bit with this method to see how it works
+
+"""
+
+from text import random_text
+# Importing a list of strings from the text.py file
+# Feel free to open that file and take a look at the list
+# Notice how it has been 'tokenized', meaning that every word, number or punctuation is an item of the list
+
