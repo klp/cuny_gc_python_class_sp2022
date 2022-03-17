973bc44d0e0fe187e18189a16f653333c0a1e956
Kai Prenger <kaiprenger@bom.local>
2022-03-17 10:19:36 -0400

    only lower function

--- home_work/2022-03-21/loop_exercise.py
+++ home_work/2022-03-21/loop_exercise.py
@@ -1,5 +1,7 @@
-""" Create a function called 'only_lower' that takes a list of strings as an argument
-and returns another list. This new list will contain only the lowered version of the words of the previous list (exclude numbers and punctuation)
+""" Create a function called 'only_lower' that takes a list of strings as an 
+argument
+and returns another list. This new list will contain only the lowered version of
+ the words of the previous list (exclude numbers and punctuation)
 
 Hint:
 - google "python isalpha" and play a bit with this method to see how it works
@@ -9,5 +11,15 @@ Hint:
 from text import random_text
 # Importing a list of strings from the text.py file
 # Feel free to open that file and take a look at the list
-# Notice how it has been 'tokenized', meaning that every word, number or punctuation is an item of the list
+# Notice how it has been 'tokenized', meaning that every word, number or 
+# punctuation is an item of the list
 
+def only_lower (some_list):
+    lowered_list = []
+    for item in some_list:
+        if item.isalpha():
+            lower_item = item.lower()
+            lowered_list.append(lower_item)
+    return lowered_list
+
+print(only_lower(random_text))
