16de0176e87b077cd1077e2c9d43eaccc8c81cc3
Kai Prenger <kaiprenger@bom.local>
2022-03-17 16:57:20 -0400

    set up for group work

---
+++ group_work/loop_exercise.py
@@ -0,0 +1,25 @@
+""" Create a function called 'only_lower' that takes a list of strings as an 
+argument
+and returns another list. This new list will contain only the lowered version of
+ the words of the previous list (exclude numbers and punctuation)
+
+Hint:
+- google "python isalpha" and play a bit with this method to see how it works
+
+"""
+
+from text import random_text
+# Importing a list of strings from the text.py file
+# Feel free to open that file and take a look at the list
+# Notice how it has been 'tokenized', meaning that every word, number or 
+# punctuation is an item of the list
+
+# create a function "only_lower"
+# initialize an empty list to store results of list loop
+# create loop structure
+# condition only add an item from the list if string is alphabetical
+# need to convert item to lower case
+# add items that first the condition in lower case to empty list
+# return new list based on the above
+
+# finally, print results of "only_lower" taking random_text as argument
