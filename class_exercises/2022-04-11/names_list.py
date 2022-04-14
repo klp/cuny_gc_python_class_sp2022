2517073a0a63aebad0c28b45b795e8209ab04932
Kai Prenger <kai.prenger@gmail.com>
2022-04-14 10:30:15 -0400

    fixed problem with not enough names

---
+++ class_exercises/2022-04-11/names_list.py
@@ -0,0 +1,10 @@
+students =["Giovanna Fuller", "Pierre Baxter", "Maxim Walker", "Dale Fuller", 
+"Keshawn Campbell", "Chaya Mills", "Alyson Grant", "Lindsay Glover", 
+"Carson Mcbride", "Zechariah Pitts", "Davon Trujillo", "Annabel Villegas", 
+"Johnathon Dickerson", "Alexa Mullins", "Lucas Ray", "Bailey Todd", 
+"Brooks Ewing", "Malaki Campos", "Annabel Campbell", "Desmond Davis", 
+"Max Blackburn", "Alivia Key", "Alvin Massey", "Marc Riddle", 
+"Zachariah Bender", "Lea Velez", "Desmond Hines", "Amir Villegas", "Asia Floyd", 
+"Ahmad Barajas", "Paige Vang", "Taylor Logan", "Ahmed Duran", "Brielle Manning", 
+"Elianna Shepard", "Leon Murillo", "Camryn Bradley", "Henry Kemp", 
+"Hazel Hancock", "Vivian Gallegos"]
