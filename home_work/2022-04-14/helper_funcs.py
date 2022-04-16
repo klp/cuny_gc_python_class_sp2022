e5817298aa2d12ccd21efff97bb21c30e0c68851
Kai Prenger <kai.prenger@gmail.com>
2022-04-16 17:59:10 -0400

    fin, for now

--- home_work/2022-04-14/helper_funcs.py
+++ home_work/2022-04-14/helper_funcs.py
@@ -1,4 +1,5 @@
 import nltk
+from nltk.corpus import stopwords
 
 def raw_to_tokens(raw_file):
     to_string = raw_file.decode()
@@ -14,6 +15,17 @@ def first_fifty_k(list):
 def filter_for_words(book, list):
     return [x for x in book if x in list]
 
-def filter_out_words(book, list):
-    return [x for x in book if x not in list]
+def remove_stops(list):
+    stop_words_en = stopwords.words('english')
+    return [w for w in list if w not in stop_words_en]
+
+def isolate_pos(l, str):
+    return list(filter(lambda x:x[1]==str,l))
+    # Note that this funciton returns a list of tuples, 
+    # which is fine for our purposes of counting POS
+    
+
+# def filter_out_words(book, list):
+#     return [x for x in book if x not in list]
+# Didn't use, but seems like it could be helpful in some circumstances
     
